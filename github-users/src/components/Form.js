ad740d15d2624cf0cbf9afc966c272adf3a0a909
Olatunji Omisakin <msknolatunji@gmail.com>
2021-05-27 12:56:18 +0100

    Getting value from input element

--- github-users/src/components/Form.js
+++ github-users/src/components/Form.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class Form extends React.Component {
+  state = {
+    userName: '',
+  };
+
+  userNameInput = React.createRef();
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      userName: event.target.value,
+    });
+    console.log(this.userNameInput.current.value);
+  };
+
   render() {
     return (
-      <form className='form'>
-        <input type='text' placeholder='Github Users' />
+      <form className='form' onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Github Users'
+          required
+          ref={this.userNameInput}
+        />
         <button>Add Card</button>
       </form>
     );
